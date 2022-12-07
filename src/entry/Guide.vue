72b2dd97628ce68d9b9ebcf2294c0195b05b5af0
wuchuang <autism_wy@163.com>
2022-12-07 11:31:48 +0800

    haha

--- src/entry/Guide.vue
+++ src/entry/Guide.vue
@@ -1,57 +1,58 @@
 <template>
-  <div class="app-guide">
-    <span>{{ title }}</span>
-  </div>
+    <div class="app-guide">
+        <span>{{ title }}</span>
+    </div>
 </template>
 <script>
-import defaultSettings from '@/setting' 
-export default {
-  name: 'appGuide',
-  components: {  },
-  data () {
-    return {
-      title: defaultSettings.title,
+    import defaultSettings from '@/setting'
+    export default {
+        name: 'appGuide',
+        components: {},
+        data() {
+            return {
+                //   title: defaultSettings.title,
+                title: '欢迎使用 ^_^'
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+
+        },
+        beforeCreate() {
+
+        },
+        created() {
+
+        },
+        beforeMount() {
+
+        },
+        mounted() {
+
+        },
+        beforeDestroy() {
+
+        },
     }
-  },
-  computed: {
-
-  },
-  methods: {
-
-  },
-  beforeCreate () {
-
-  },
-  created () {
-
-  },
-  beforeMount () {
-
-  },
-  mounted () {
-
-  },
-  beforeDestroy() {
-
-  },
-}
 </script>
 <style lang="scss" scoped>
-.app-guide {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  
-  background-size: cover;
-  // filter: opacity(70%);
+    .app-guide {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
 
-  span {
-    color: var(--color-info);
-    letter-spacing: 2px;
-    font-size: 50px;
-    margin-bottom: 35%;
-  }
-}
+        background-size: cover;
+        // filter: opacity(70%);
+
+        span {
+            color: var(--color-info);
+            letter-spacing: 2px;
+            font-size: 50px;
+            margin-bottom: 10%;
+        }
+    }
 </style>
